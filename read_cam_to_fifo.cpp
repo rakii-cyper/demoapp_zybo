e1aa366ceed7b29636a7934e4bec508a3fbe6682
root <root@localhost.localdomain>
2022-10-28 15:42:10 +0000

    update 006

--- read_cam_to_fifo.cpp
+++ read_cam_to_fifo.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[]) {
   }
 
   // config_console(); // Configure standard input not to wait for CR
-  VideoCapture cap(0); 
+  VideoCapture cap("test.avi"); 
     
   // Check if camera opened successfully
   if(!cap.isOpened()){
