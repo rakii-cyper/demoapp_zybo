2fea04a14afcc1dbfe27119fc08bc33d475430e4
rakii-cyper <19521007@gm.uit.edu.vn>
2022-10-28 15:28:31 +0700

    Update read_fifo_to_cam.cpp

--- read_fifo_to_cam.cpp
+++ read_fifo_to_cam.cpp
@@ -73,7 +73,7 @@ int main(int argc, char *argv[]) {
       for(int k = 0; k < 3; k++) {
         intensity.val[k] = int(buf[len + k]);
       }
-      cout << "reading [" << cols << ", " << rows << "]" << endl;
+      // cout << "reading [" << cols << ", " << rows << "]" << endl;
       cols++;
       len = len + 4;
       if (cols == 640) {
