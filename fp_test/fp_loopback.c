70247f64a05c57a40b30ce34d785d74707937a4c
rakii-cyper <19521007@gm.uit.edu.vn>
2022-11-09 02:11:17 +0700

    Update fp_loopback.c

--- fp_test/fp_loopback.c
+++ fp_test/fp_loopback.c
@@ -12,13 +12,12 @@
 #include <string.h>
 #include <semaphore.h>
 #include <sys/mman.h>
-#include <dirent.h>
 
 #define NUMBER_OF_FRAME 128
 
 struct arg_struct {
     char *device_name;
-    char *root_dir_path;
+    char *file_name;
 };
 
 void allwrite(int fd, float *buf, int len);
@@ -30,25 +29,25 @@ int main(int argc, char *argv[]) {
   struct arg_struct arg;
 
   if (argc!=4) {
-    fprintf(stderr, "Usage: %s read_devfile write_devfile input_root_dir\n", argv[0]);
+    fprintf(stderr, "Usage: %s read_devfile write_devfile input_file\n", argv[0]);
     exit(1);
   }
 
   arg.device_name = argv[2];
-  arg.root_dir_path = argv[3];
-  write_to_fifo((void *) &arg);
-  // if (pthread_create(&thread_id[0], NULL, read_from_fifo, (void *) argv[1])) {
-  //   perror("Failed to create thread");
-  //   exit(1);
-  // }
+  arg.file_name = argv[3];
+  // write_to_fifo((void *) &arg);
+  if (pthread_create(&thread_id[0], NULL, read_from_fifo, (void *) argv[1])) {
+    perror("Failed to create thread");
+    exit(1);
+  }
 
-  // if (pthread_create(&thread_id[1], NULL, write_to_fifo, (void *) &arg)) {
-  //   perror("Failed to create thread");
-  //   exit(1);
-  // }
+  if (pthread_create(&thread_id[1], NULL, write_to_fifo, (void *) &arg)) {
+    perror("Failed to create thread");
+    exit(1);
+  }
 
-  // // pthread_join(thread_id[0], NULL);
-  // pthread_join(thread_id[1], NULL);
+  pthread_join(thread_id[0], NULL);
+  pthread_join(thread_id[1], NULL);
 
   return -1;
 }
@@ -128,64 +127,43 @@ void *write_to_fifo(void* arg) {
   struct arg_struct *arguments = (struct arg_struct *) arg;
   int fd;
   float buf[NUMBER_OF_FRAME];
-  // initial variables for listing root direction reading input file
-  DIR *dp;
-  struct dirent *ep;  
-  printf("%s", arguments->root_dir_path);
-  // dp = opendir (arguments->root_dir_path);
-  // fd = open(arguments->device_name, O_WRONLY);
 
-  // if (fd < 0) {
-  //   if (errno == ENODEV)
-  //     fprintf(stderr, "(Maybe %s a read-only file?)\n", arguments->device_name);
+  // initial variables for reading input file
+  FILE * fp;
+  char * line = NULL;
+  size_t len = 0;
+  ssize_t read_cnt;
+  int counter = 0;
 
-  //   perror("Failed to open write devfile");
-  //   exit(1);
-  // }
-    
-  if (dp != NULL) {
-    while ((ep = readdir (dp)) != NULL) {
-      if (strcmp(ep->d_name, ".") != 0 || strcmp(ep->d_name, "..") != 0) {
-        // initial variables for reading input file
-        printf("START");
-        FILE * fp;
-        char * line = NULL;
-        size_t len = 0;
-        ssize_t read_cnt;
-        int counter = 0;
+  fp = fopen(arguments->file_name, "r");
+  fd = open(arguments->device_name, O_WRONLY);
+  
+  if (fd < 0) {
+    if (errno == ENODEV)
+      fprintf(stderr, "(Maybe %s a read-only file?)\n", arguments->device_name);
 
-        char * file_name = NULL;
-        strcpy(file_name, arguments->root_dir_path);
-        strcat(file_name, "/");
-        strcat(file_name, ep->d_name);
-        printf("%s", file_name);
-        // fp = fopen(file_name, "r");
-
-        // if (fp == NULL) {
-        //   perror("Failed to open input file");
-        //   exit(1);
-        // }
-
-        // while ((read_cnt = getline(&line, &len, fp)) != -1) {
-        //   // Read from standard input = file descriptor 0
-        //   if (read_cnt != 0) {
-        //     line[strlen(line)-1] = '\0';
-        //     buf[counter++] = atof(line);
-        //   }
-        //   // printf("Retrieved line of length %zu:\n", read_cnt);
-        //   // printf("%s", line);
-        // }
-        // allwrite(fd, buf, counter*4);
-        // fclose(fp);
-        // if (line)
-        //   free(line);
-        // printf("DONE WRITING!!!\n");
-      }
-    }
-    (void) closedir (dp);
-    return NULL;
-  } else {
-    perror ("Couldn't open the directory");
-    exit(-1);
+    perror("Failed to open write devfile");
+    exit(1);
   }
+
+  if (fp == NULL) {
+    perror("Failed to open input file");
+    exit(1);
+  }
+
+  while ((read_cnt = getline(&line, &len, fp)) != -1) {
+    // Read from standard input = file descriptor 0
+    if (read_cnt != 0) {
+      line[strlen(line)-1] = '\0';
+      buf[counter++] = atof(line);
+    }
+    // printf("Retrieved line of length %zu:\n", read_cnt);
+    // printf("%s", line);
+  }
+  allwrite(fd, buf, counter*4);
+  fclose(fp);
+  if (line)
+    free(line);
+  printf("DONE WRITING!!!\n");
+  return NULL;
 }
